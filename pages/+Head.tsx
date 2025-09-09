1ce594af862a236bdc9c8c3047b84c51151bbd58
Tiina Koskiranta <tiina.koskiranta@gmail.com>
2025-09-09 16:27:14 +0300

    add vike starter content (vite, vike, eslint, prettier, ts etc)

---
+++ pages/+Head.tsx
@@ -0,0 +1,8 @@
+// https://vike.dev/Head
+
+//# BATI.has("mantine")
+import logoUrl from "../assets/logo.svg";
+
+export default function HeadDefault() {
+  return <link rel="icon" href={logoUrl} />;
+}
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import vercel from "vite-plugin-vercel";
+import react from "@vitejs/plugin-react";
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [vike(), react(), vercel()],
+  build: {
+    target: "es2022",
+  },
+});
