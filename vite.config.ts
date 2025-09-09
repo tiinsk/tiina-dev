ef766c792ef9896fdaa8bd479d13b4a04518e857
Tiina Koskiranta <tiina.koskiranta@gmail.com>
2025-09-09 15:53:34 +0300

    remove incompatible vite vercel plugins (for now)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import vercel from "vite-plugin-vercel";
+//import vercel from "vite-plugin-vercel";
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vike(), react(), vercel()],
+  plugins: [vike(), react(), /*vercel()*/],
   build: {
     target: "es2022",
   },
